0f4633499ccd7eb6456057d42267529e7789f84a
harrisonWAffel <affelh@wit.edu>
2020-06-09 21:46:13 -0400

    fix sql files, permissions, and docker networking

--- golangRpc/main.go
+++ golangRpc/main.go
@@ -3,14 +3,27 @@ package main
 import (
 	userAuth "./controller/UserAuthentication"
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/prometheus/common/log"
 	"net/http"
+	"time"
+)
+const (
+	host = "ASeeker-transcription-database"
+	//host = "tcp(127.0.0.1)"
+
+	port     = 3306
+	user     = "root"
+	password = "toor"
+	dbname   = "aseeker"
 )
 
-var Database, err = sql.Open("mysql", "root:toor@tcp(127.0.0.1:3306)/aseeker")
+var psqlInfo = fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", user,password, host, dbname)
 
 func main() {
+	time.Sleep(10*time.Second)
+	var Database, err = sql.Open("mysql", psqlInfo)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -18,6 +31,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 
 	log.Info("Setting Service Up On Port 1177")
 	mux := http.DefaultServeMux
