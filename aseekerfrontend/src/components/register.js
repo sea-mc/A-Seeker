c23f584b459b919b13ee89785b5b1dcc48d161f7
harrisonWAffel <affelh@wit.edu>
2020-06-12 13:30:45 -0400

    Implement proper registration form which is able to make requests to the middleware

--- aseekerfrontend/src/components/register.js
+++ aseekerfrontend/src/components/register.js
@@ -1,32 +1,71 @@
 import React, {Component} from 'react';
 import './css/bodyContent.css'
 import './css/bootstrap.css';
-import bodyContent from "./bodyContent";
+
 
 class Register extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { email: '' , password: ''};
+    }
+
+    mySubmitHandler = (event) => {
+        event.preventDefault();
+
+        var requestOptions = {
+                    method: 'POST',
+                    redirect: 'follow'
+                };
+                const data = new FormData(event.email);
+                console.log(data);
+                console.log("http://localhost:1177/udserauth/register/new?email="+this.state.email +"&password="+this.state.password);
+                fetch("http://localhost:1177/userauth/register/new?email="+this.state.email +"&password="+this.state.password, requestOptions)
+                    .then(response => response.text())
+                    .then(result => function(){
+                        console.log(result);
+                        console.log(result.status);
+
+                    })
+                    .catch(error => console.log('error', error));
+
+    };
+
+
+    emailChangeHandler = (event) => {
+      this.setState({email: event.target.value})
+    };
+
+
+    passwordChangeHandler = (event) => {
+        this.setState({password: event.target.value})
+    };
+
+
     render() {
         return (
-            <form className={bodyContent()}>
+            <div>
                 <h3>Sign In</h3>
 
-                <div className="form-group">
+                <form onSubmit={this.mySubmitHandler}  className="form-group">
+
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
+                    <input type="email" className="form-control" placeholder="Enter email" onChange={this.emailChangeHandler}/>
 
-                <div className="form-group">
+
+
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
+                    <input type="password" className="form-control" placeholder="Enter password" onChange={this.passwordChangeHandler}/>
 
-                <div className="form-group">
+
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Confirm password" />
-                </div>
+                    <input type="password" className="form-control" placeholder="Confirm password" onChange={this.passwordChangeHandler}/>
 
-
+        <div className="form-row">
                 <button type="submit" className="btn btn-primary btn-block">Register</button>
-            </form>
+        </div>
+                </form>
+            </div>
         );
     }
 }
