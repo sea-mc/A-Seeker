c74db924bcfcc372ded9dc3038c2a4032a59fd63
seanLifePod <sean@lifepod.com>
2020-06-19 15:53:27 -0400

    update API in python scripts, and add some test DB connnections

---
+++ deepspeech/aseeker_api.py
@@ -0,0 +1,40 @@
+from deepspeech.database.transcription_db import connect_to_db
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '~/audio'
+ALLOWED_EXTENSIONS = {'wav', 'mp3', 'mp4'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if __name__ == '__main__':
+    connect_to_db()
+    app.run()
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return 200
+
+
---
+++ deepspeech/aseeker_controller.py
@@ -0,0 +1,13 @@
+from deepspeech.DeepSpeech.transcribe import *
+
+
+def transcribe_input(filepath, filename):
+    if ".wav" not in filename:
+        format_file(filename)
+    transcribe_one(filepath, "/transcriptions/" + filename)
+
+
+def format_file(filename):
+    proper_filetypes = [".mp3", ".mp4"]
+    if not any(ext in filename for ext in proper_filetypes):
+        print("Invalid filetype {}", filename)
