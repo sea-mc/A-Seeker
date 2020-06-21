7b91214562e2eff9cf282e12eef749980575f3a6
coxs4 <coxs4@wit.edu>
2020-06-21 19:25:13 -0400

    got 201 from local testing when uploading file, pushing to test UI interaction

--- deepspeech/aseeker_api.py
+++ deepspeech/aseeker_api.py
@@ -20,28 +20,19 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    # check if the post request has the file part
-    if '.wav' not in request.files:
-        resp = jsonify({'message': 'No wav file part in the request'})
-        resp.status_code = 400
-        return resp
-    file = request.files['file']
-    if file.filename == '':
-        resp = jsonify({'message': 'No file selected for uploading'})
-        resp.status_code = 400
-        return resp
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        resp = jsonify({'message': 'File successfully uploaded'})
-        resp.status_code = 201
-        return resp
-    else:
-        resp = jsonify({'message': 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
-        resp.status_code = 400
-        return resp
+@app.route('/upload/<filename>', methods=['POST'])
+def upload_file(filename):
+    """Upload a file."""
+
+    if "/" in filename:
+        # Return 400 BAD REQUEST
+        os.abort(400, "no subdirectories directories allowed")
+
+    with open(os.path.join(AUDIO_FOLDER, filename), "wb") as fp:
+        fp.write(request.data)
+
+    # Return 201 CREATED
+    return "", 201
 
 
 @app.route('/user/get-transcriptions', methods=['GET'])
@@ -51,3 +42,25 @@ def get_transcriptions():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='1178')
+
+
+# # check if the post request has the file part
+#     if '.wav' not in request.files:
+#         resp = jsonify({'message': 'No wav file part in the request'})
+#         resp.status_code = 400
+#         return resp
+#     file = request.files['file']
+#     if file.filename == '':
+#         resp = jsonify({'message': 'No file selected for uploading'})
+#         resp.status_code = 400
+#         return resp
+#     if file and allowed_file(file.filename):
+#         filename = secure_filename(file.filename)
+#         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#         resp = jsonify({'message': 'File successfully uploaded'})
+#         resp.status_code = 201
+#         return resp
+#     else:
+#         resp = jsonify({'message': 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+#         resp.status_code = 400
+#         return resp
