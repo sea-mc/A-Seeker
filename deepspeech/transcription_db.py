5e6106fdc451455b57e4723a584a11deedd39998
coxs4 <coxs4@wit.edu>
2020-06-21 21:09:59 -0400

    db end edit

--- deepspeech/transcription_db.py
+++ deepspeech/transcription_db.py
@@ -1,17 +1,5 @@
-import configparser
-from database.aseeker_config import *
-from flask import Flask
 import pymysql
-import time
 
-
-db = Flask(__name__)
-
-
-db.config['MYSQL_HOST'] = 'localhost'
-db.config['MYSQL_USER'] = 'root'
-db.config['MYSQL_PASSWORD'] = 'toor'
-db.config['MYSQL_DB'] = 'aseeker'
 
 class Database:
     def __init__(self):
@@ -20,11 +8,12 @@ class Database:
         password = 'toor'
         db = 'aseeker'
 
-        self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.DictCursor)
+        self.con = pymysql.connect(host=host, user=user, password=password, db=db,
+                                   cursorclass=pymysql.cursors.DictCursor)
         self.cur = self.con.cursor()
 
     def insert_transcription(self, email, preview, full_transcription, audio_path, title):
-        title = title+'.wav'
+        title = title + '.wav'
 
         self.cur.execute('INSERT INTO transcription (test@test.com,Preview...,A full transcription...,/audio/title,example);')
         # POST method to insert transcription into DB
@@ -35,7 +24,3 @@ class Database:
         result = self.cur.fetchall()
 
         return result
-
-
-
-
